cf9d4122ade4135e4f118ab24e7471efb93380d5
17kikak <kikakovaleski@gmail.com>
2019-03-08 06:59:08 -0500

    convert from celcius

--- rails-converter/app/controllers/temperature_controller.rb
+++ rails-converter/app/controllers/temperature_controller.rb
@@ -1,6 +1,13 @@
 class TemperatureController < ApplicationController
   def converter
     origTemp = params[:temp]
-    render plain: origTemp
+    if params[:scale1] === "c"
+      if params[:scale2] === "f"
+        newTemp = origTemp.to_i * 1.8 + 32
+      elsif params[:scale2] === "k"
+        newTemp = origTemp.to_i + 273.15
+      end
+    end
+    render plain: newTemp
   end
 end
