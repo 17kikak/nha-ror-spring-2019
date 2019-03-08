a24cbca363351abd8628e2aa2b331854c31c1cdb
17kikak <kikakovaleski@gmail.com>
2019-03-08 07:09:47 -0500

    everything working

--- rails-converter/app/controllers/temperature_controller.rb
+++ rails-converter/app/controllers/temperature_controller.rb
@@ -7,6 +7,18 @@ class TemperatureController < ApplicationController
       elsif params[:scale2] === "k"
         newTemp = origTemp.to_i + 273.15
       end
+    elsif params[:scale1] === "f"
+      if params[:scale2] === "c"
+        newTemp = (origTemp.to_i - 32) * 0.555
+      elsif params[:scale2] === "k"
+        newTemp = (origTemp.to_i - 32) * 0.555 + 273.15
+      end
+    elsif params[:scale1] === "k"
+      if params[:scale2] === "c"
+        newTemp = origTemp.to_i - 273.15
+      elsif params[:scale2] === "f"
+        newTemp = (origTemp.to_i - 273.15) * 1.8 + 32
+      end
     end
     render plain: newTemp
   end
